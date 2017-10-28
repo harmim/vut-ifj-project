461eb0ab97ffa55bfc260bd2b519b4ff11096f4e
CapainUnitato <unitato@narnia>
2017-10-28 19:31:48 +0200

    #3 Analysis skeleton

---
+++ analysis.c
@@ -0,0 +1,7 @@
+#include "analysis.h"
+#include "scanner.h"
+#include <stdio.h>
+
+Token token;
+extern FILE* source_file;
+
---
+++ analysis.h
@@ -0,0 +1,20 @@
+﻿/**
+* Project: Implementace překladače imperativního jazyka IFJ17.
+*
+* @brief Syntactical and semantical analysis
+* @author Matej karas <xkaras34@stud.fit.vutbr.cz>
+*/
+
+
+#ifndef _ANALYSIS_H
+#define _ANALYSIS_H
+
+#define SYNTAX_OK					0
+#define SYNTAX_ERR					2
+#define SEM_ERR_UNDEFINED_VAR		3
+#define SEM_ERR_TYPE_COMPAT			4
+#define SEM_ERR_OTHER				6
+
+
+
+#endif
